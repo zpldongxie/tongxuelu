6df22f5f4748c7baa2500820a8d42041ea9b330b
pengliang.zhu <pengliang.zhu@clouddeep.cn>
2019-09-26 16:16:24 +0800

    撤消知道创宇云挂标验证

--- src/Template/Layout/index.js
+++ src/Template/Layout/index.js
@@ -2,7 +2,7 @@
  * @Description: 公共布局模板
  * @Author: zpl
  * @Date: 2019-09-18 21:46:58
- * @LastEditTime: 2019-09-25 16:47:06
+ * @LastEditTime: 2019-09-26 16:13:45
  * @LastEditors: zpl
  */
 import React, { Component } from 'react'
@@ -17,12 +17,13 @@ export default class Layout extends Component {
         const { children } = this.props
         return (
             <div className='rootCon' style={{ background: `url(${treeImg}) no-repeat center`, backgroundSize: 'cover' }}>
+                {children}
                 <ReactAudioPlayer
                     src={bgMusic}
                     autoPlay
                     loop
+                    volume={0.5}
                 />
-                {children}
             </div>
         )
     }
